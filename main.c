54b8761b07fa5ab1c4d48fce8ce35323f9aaf7e3
Michael Gorsuch <michael.gorsuch@gmail.com>
2014-03-20 20:06:23 +0000

    require input

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <curl/curl.h>
  
-int main(void)
+int main(int argc, char * argv[])
 {
   CURL *curl;
   CURLcode res;
+  char *url;
 
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s url\n", argv[0]);
+    return 1;
+  }
+  url = argv[1];
   curl = curl_easy_init();
 
   if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
+    curl_easy_setopt(curl, CURLOPT_URL, url);
     /* example.com is redirected, so we tell libcurl to follow redirection */ 
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
  
